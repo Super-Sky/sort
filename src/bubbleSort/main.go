081a9a1a650b981c0ec25fa942f9660d5a97d3da
domyselfzhen123 <993566949@qq.com>
2020-08-12 19:07:27 +0800

    冒泡排序

---
+++ src/bubbleSort/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+//冒泡排序：
+//通过对序列从后往前,依次比较相邻元素的排序码，若发现逆序则交换，使排序码较小的元素逐渐从后向前移动。
+
+
+func BubbleSort(arr *[5]int) {
+	lenArr := len(*arr)
+	for i:=0;i<lenArr-1;i++ {
+		for j:=0;j<lenArr-i-1;j++ {
+			if (*arr)[j] > (*arr)[j+1] {
+				(*arr)[j],(*arr)[j+1] = (*arr)[j+1],(*arr)[j]
+			}
+		}
+	}
+}
+
+func main() {
+	arr := [5]int{5,1,9,24,11}
+	fmt.Printf("排序前：%v\n",arr)
+	BubbleSort(&arr)
+	fmt.Printf("排序后：%v\n",arr)
+}
